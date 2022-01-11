6d922680f7feb40cd043d49c085abc0d788288fe
Fotios Tsotas <92823972+FotisTsotas@users.noreply.github.com>
2022-01-11 16:35:38 +0200

    Dynamic posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,78 +1,24 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
 
-class Post
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    public $title;
+    use HasFactory;
 
-    public $excerpt;
+    protected $fillable = ['slug','title','excerpt','body'];
 
-    public $date;
-
-    public $body;
-
-    public $slug;
-
-    public function __construct($title, $excerpt, $date, $body, $slug)
+    public function getRouteKeyName()
     {
-        
-        $this->title = $title;
-
-        $this->excerpt = $excerpt;
-
-        $this->date = $date;
-
-        $this->body = $body;
-        
-        $this->slug = $slug;
-
-
-
-
+        return 'slug';
     }
-    public static function all()
+
+    public function category()
     {
-        return cache()->rememberForever('posts.all', function (){
-           
-            return collect( File::files(resource_path("posts")))
-            ->map(fn($file) => YamlFrontMatter::parseFile($file))
+        return $this->belongsTo(Category::class);
 
-            ->map(fn($document) => new Post(
-            $document->title,
-
-            $document->excerpt,
-
-            $document->date,
-
-            $document->body(),
-
-            $document->slug,
-            ))
-            ->sortByDesc('date');
-    
-
-        });
-    }   
-
-public static function find($slug)
-    {
- 
-    return static::all()->firstWhere('slug', $slug);
-
-}
-
-public static function findOrFail($slug)
-    {
- 
-    $post = static::find($slug);
-    if (! $post){
-        throw new ModelNotFoundException();
-    }
-    
-    return $post;
     }
 }
