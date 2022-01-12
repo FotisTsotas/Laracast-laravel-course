8db16aee26aaa2eb4fef517c2783d9cb16c4d52d
Fotios Tsotas <92823972+FotisTsotas@users.noreply.github.com>
2022-01-12 17:00:47 +0200

    32chapter

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,23 +1,27 @@
 <x-layout>
-@foreach ($posts as $post)
-    
-        <article>
-            <h1>
-                <a href="/posts/{{ $post->slug}}">
-            <h1>  {{$post->title}}</h1>            
-            </a>
-            <p>
-           By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="/categories/{{ $post->category->slug }}">{{$post->category->name}}</a>
+        @include('_posts-header')
 
-            </p>
-            <div>
-                <p>
-                {{$post->excerpt}}
-                </p>
-            </div>
-        </article>
-    
-    
-    @endforeach
+        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+            @if ($posts->count())
+                <x-post-featured-card :post="$posts[0]" />
+
+                @if ($posts->count() > 1)
+                 <div class="lg:grid lg:grid-cols-2">
+                        @foreach ($posts->skip(1) as $post)
+                         <x-post-card :post="$post"/>
+                        @endforeach     
+                 </div>  
+                 @endif
+            @else
+            <p class="text-center">No posts yet.Please check back later.</p>
+            @endif
+
+                <!-- <div class="lg:grid lg:grid-cols-3">                
+                    <x-post-card/>
+                    <x-post-card/>
+                    <x-post-card/>
+            
+                </div> -->
+        </main> 
 
 </x-layout>
