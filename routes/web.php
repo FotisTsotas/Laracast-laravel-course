ec88f80b03114ccc2e3681135a550dc4d0c98519
Fotios Tsotas <92823972+FotisTsotas@users.noreply.github.com>
2022-01-13 14:59:31 +0200

    38chapter

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Models\User;
 use App\Models\Post;
 use App\Models\Category;
+use Illuminate\Support\Facades\Route;
 
 use phpDocumentor\Reflection\Types\Collection;
 
@@ -18,20 +19,8 @@ use phpDocumentor\Reflection\Types\Collection;
 |
 */
 
-Route::get('/', function () {
-    return view('posts', [
-        'posts' =>  Post::latest()->get(),
-        'categories' => Category::all()
-    ]);
-})->name('home');
-
-Route::get('posts/{post:slug}', function (Post $post) {
-
-    return view('post', [
-        'post' => $post
-    ]);
-
-});
+Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get('posts/{post:slug}',[PostController::class, 'show']);
 
 Route::get('categories/{category:slug}',function(Category $category){
     return view('posts', [
